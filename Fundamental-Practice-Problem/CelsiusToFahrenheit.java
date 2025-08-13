1489aebc2f9d56ed557cc455a572824a342889d9
Deepanshu Rajput <deepanshu.rajput9242@gmail.com>
2025-08-13 16:16:47 +0530

    donebyme

---
+++ Fundamental-Practice-Problem/CelsiusToFahrenheit.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CelsiusToFahrenheitConversion {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int Celsius=scn.nextInt();
+        int Fahrenheit=(Celsius*9/5)+32;
+        System.out.println(Fahrenheit);
+    }
+}
---
+++ Fundamental-Practice-Problem/PerimeterOfRectangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VolumeOfACylinder {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int radius=scn.nextInt();
+        int height=scn.nextInt();
+        double volume=3.14*radius*radius*height;
+        System.out.println(volume
+
+
+
+        );
+    }
+}
+
