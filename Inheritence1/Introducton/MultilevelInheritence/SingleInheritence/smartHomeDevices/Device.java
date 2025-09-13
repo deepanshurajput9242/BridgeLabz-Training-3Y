229bc1d8c9ecbd3d3e64dfe2433dcdcf9a6d87f4
Deepanshu Rajput <deepanshu.rajput9242@gmail.com>
2025-09-13 23:21:40 +0530

    [Deepanshu] Added Device

---
+++ Inheritence1/Introducton/MultilevelInheritence/SingleInheritence/smartHomeDevices/Device.java
@@ -0,0 +1,30 @@
+package com.smartHomeDevices.gla;
+public class Device {
+    String deviceId;
+    String status;
+    Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+// Subclass
+class Thermostat extends Device {
+    int temperatureSetting;
+
+    Thermostat(String deviceId, String status, int temperatureSetting) {
+        super(deviceId, status); // Call Device constructor
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
